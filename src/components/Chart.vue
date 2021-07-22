cbc8bc16a29c974a2d74a06194ff7094b10ac1e5
kt_boom <kritsada.tan@hotmail.com>
2021-07-23 00:41:43 +0700

    add records & chart

---
+++ src/components/Chart.vue
@@ -0,0 +1,29 @@
+
+
+<script>
+// import VueCharts from 'vue-chartjs'
+import { Bar } from 'vue-chartjs'
+export default {
+  extends: Bar,
+  props: {
+    chartData: Array,
+  },
+  mounted () {
+    // Overwriting base render method with actual data.
+    this.renderChart({
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+      datasets: [
+        {
+          label: 'Your Remaining in 2021',
+          backgroundColor: '#f87979',
+          data: this.chartData
+        }
+      ]
+    }, {responsive: true, maintainAspectRatio: false})
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,15 +2,17 @@
   <div class="Home">
     <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
     <h1>Income and Expense</h1>
-    <div btn-wrap v-if="state === 0">
+    <div class="btn-wrap" v-if="state === 0">
       <button v-on:click="addIncome">Add Income</button>
       <button v-on:click="addExpense">Add Expense</button>
     </div>
+    <div class="chart-wrap">
+      <button v-if="state === 0 && !chartDisplay" v-on:click="showChart">Chart</button>
+      <chart v-if="state === 0 && chartDisplay" :chartData="calChartData()"/>
+      <button v-if="state === 0 && chartDisplay" v-on:click="showChart">Back</button>
+    </div>
     <record-create-form v-if="state !== 0" :state="state" @update="disappear" />
     <income-expense  :totalList="totalList=[]" :records="records"/>
-    
-    <graph/>
-    
   </div>
 </template>
 
@@ -18,7 +20,7 @@
 // @ is an alias to /src
 import IncomeExpense from "../components/IncomeExpenseTable.vue";
 import RecordCreateForm from "../components/RecordCreateForm.vue";
-import Graph from "../components/Graph.vue";
+import Chart from "../components/Chart.vue";
 import RecordStore from "@/store/record";
 
 export default {
@@ -26,21 +28,19 @@ export default {
   components: {
     IncomeExpense,
     RecordCreateForm,
-    Graph,
+    Chart,
   },
   data() {
     return {
       state: 0,
       totalList: [],
       records: [],
-
+      chartDisplay: false,
     };
   },
   created() {
-    
     this.fetchRecord();
   },
-  
   methods: {
     fetchRecord() {
       RecordStore.dispatch("fetchRecord");
@@ -54,16 +54,26 @@ export default {
     },
     disappear(state) {
       this.state = state;
-      //this.forceRerender()
     },
-    // forceRerender() {
-    //   this.renderComponent = false;
-
-    //   this.$nextTick(() => {
-          
-    //       this.renderComponent = true;
-    //     });
-    // }
+    showChart() {
+      this.chartDisplay = !this.chartDisplay
+    },
+    calChartData() {
+      var chartData = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+      // total remain in that month
+      this.records.map(record => {
+        if(record.date.slice(0, 4) === "2021"){
+          // console.log(parseInt(record.date.slice(5, -3)));
+          let index = parseInt(record.date.slice(5, -3)) - 1
+          if(record.type === "Income")
+            chartData[index] += record.amount 
+          if(record.type === "Expense")
+            chartData[index] -= record.amount
+        }
+      })
+      return chartData;
+    }
+    
   },
 };
 </script>
